d517773ef0ad99374cacf338ffc7431df471bf05
boody.fci <boody.fci@1202452c-5e50-11de-b59c-372aa07dcf7f>
2009-06-29 23:06:48 +0000

    drawing Circles when clicking the Mouse

--- trunk/src/graphics/FrameAndBackground.java
+++ trunk/src/graphics/FrameAndBackground.java
@@ -1,15 +1,15 @@
 package graphics;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
-import sun.java2d.loops.DrawRect;
-
-public class FrameAndBackground {
+public class FrameAndBackground extends JFrame{
 	public static void main(String[] args) {
 //		Creating the Frame and setting it's properties [visibility, size, resizability and closing]
-		JFrame frame = new JFrame("Connect 4");
+		FrameAndBackground frame = new FrameAndBackground();
 		frame.setVisible(true);
 		frame.setSize(800,600);
 		frame.setResizable(false);
@@ -18,8 +18,37 @@ public class FrameAndBackground {
 //		Adding the JPanel to the JFrame and edit their properties
 		Panel panel = new Panel();
 		panel.setOpaque(false);
-		frame.add(panel,BorderLayout.CENTER);
+		frame.add(panel, BorderLayout.CENTER);
 		frame.pack();
-
 	}
+
+//	getting the mouse position when clicked
+	int x, y;
+	public FrameAndBackground() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent mouseClick) { 
+				x=mouseClick.getX();
+				y=mouseClick.getY();
+				repaint();
+			}
+		});
+	}
+	
+//	Painting the Circles and filling them
+	public void paint(Graphics g) {
+		super.paint(g);
+//		Drawing the Circles
+		for (int i=1;i<=7;i++) {
+			for (int j=1;j<=6;j++) {
+				g.setColor(Color.white);
+				g.drawOval(130+(i*60), (j*60), 50, 50);
+
+				if (x>=130+(i*60) && x<=180+(i*60)) {
+					if (y>=(j*60) && y<=50+(j*60)) {
+						g.fillOval(130+(i*60), (j*60), 50, 50);
+					}
+				}
+			}
+		}
+	}	
 }
--- trunk/src/graphics/Panel.java
+++ trunk/src/graphics/Panel.java
@@ -1,31 +1,23 @@
 package graphics;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 class Panel extends JPanel{
 	final Image image = new ImageIcon("src/images/Connect4_bg.png").getImage();
-
+	
+	public Panel(){
+		repaint();
+	}
+	
+//	Drawing the image
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		// Drawing the image
-		g.drawImage(image, 0, 0, null);
+		g.drawImage(image, 0, 0, this);
+	}	
 
-		// Drawing the Circles
-		for (int i=1;i<=7;i++) {
-			for (int j=1;j<=6;j++) {
-				g.setColor(Color.white);
-				g.drawOval(150+(i*55), (j*55), 50, 50);
-			}
-		}
-	}
-
-	// Setting the JPanel's Size
+//	Setting the JPanel's Size
     public Dimension getPreferredSize() {
         return new Dimension(800,600);
     }
