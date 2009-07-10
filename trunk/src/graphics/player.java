d808aed4b617e94aa4b036b6245004931d91cbe2
tmtm_7lmy2007@yahoo.com <tmtm_7lmy2007@yahoo.com@1202452c-5e50-11de-b59c-372aa07dcf7f>
2009-07-10 21:53:27 +0000

    this player class

---
+++ trunk/src/graphics/player.java
@@ -0,0 +1,15 @@
+public interface Player  //this class to define player and it is interface to use in score code
+{
+
+	 public Move getMove(Board b); // asked what move it would like to make  this method i do in Asynchronous class
+
+
+     
+    public String getName(); //  Return the name of the player.
+  
+   
+    public int getNumber();//  Get the players number
+  
+  
+}
+
