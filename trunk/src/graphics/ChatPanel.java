41daa4a914313bbf4588427f24afb632c34d5ac9
boody.fci <boody.fci@1202452c-5e50-11de-b59c-372aa07dcf7f>
2009-08-06 00:28:50 +0000

    1- Merged Client.java with DrawingOvals.java 2- Merged Server.java with DrawingOvals.java in new file 3- Added new class ChatPanel.java containing the Chat Panel 4- Added two images "chat.png" and "connect4.png"

--- trunk/src/graphics/ChatPanel.java
+++ trunk/src/graphics/ChatPanel.java
@@ -1,20 +1,16 @@
 package graphics;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-class Panel extends JPanel {
+class ChatPanel extends JPanel {
 	/**
 	 * This class draws the game's Background image and sets its dimension
 	 * */
-	final Image image = new ImageIcon("src/images/Connect4_bg.png").getImage();
+	final Image image = new ImageIcon("src/images/chat.png").getImage();
 
-	public Panel() {
+	public ChatPanel() {
 		repaint();
-		// JLabel label = new JLabel("hello");
-		// label.setForeground(Color.white);
-		// add(label);
 	}
 
 	// Drawing the image
@@ -25,6 +21,6 @@ class Panel extends JPanel {
 
 	// Setting the JPanel's Size
 	public Dimension getPreferredSize() {
-		return new Dimension(800, 600);
+		return new Dimension(800, 150);
 	}
 }
--- trunk/src/graphics/DrawingOvalsServer.java
+++ trunk/src/graphics/DrawingOvalsServer.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class DrawingOvals extends JFrame implements Runnable {
+public class DrawingOvalsServer extends JFrame implements Runnable {
 	/**
 	 * This class draws Ovals (Circles) on the main frame and checks whether any
 	 * of these Ovals are being clicked on then it performs action according to
@@ -21,39 +21,36 @@ public class DrawingOvals extends JFrame implements Runnable {
 	JScrollPane sp_recieveSpace = new JScrollPane(recieveSpace);
 	JButton send = new JButton("send");
 
+	ServerSocket serverSocket = null;
 	Socket socket;
 	BufferedReader in = null;
 	BufferedWriter out = null;
 
 	public void run() {
-
 		while (true) {
 			try {
 				InetAddress ia = socket.getInetAddress();
 				recieveSpace.append(ia.getHostName() + " : " + in.readLine()
 						+ "\n");
 			} catch (IOException e) {
-
 			}
 		}
 	}
 
 	public void mainFrame() {
-
 		// Creating the Frame and setting it's properties [visibility, size,
 		// resizability and closing]
-		DrawingOvals frame = new DrawingOvals();
+		DrawingOvalsServer frame = new DrawingOvalsServer();
 		frame.setVisible(true);
 		frame.setSize(800, 600);
 		frame.setTitle("Connect 4");
-		frame.setResizable(true);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
 		// Adding the JPanel to the JFrame and edit their properties
 		Panel panel = new Panel();
 		panel.setOpaque(false);
-
-		frame.add(panel, BorderLayout.NORTH);
+		frame.add(panel, BorderLayout.CENTER);
 		frame.pack();
 
 		// adding chat panel to the frame and add chat components to it
@@ -63,20 +60,19 @@ public class DrawingOvals extends JFrame implements Runnable {
 		recieveSpace.setEditable(false);
 		cp.add(sendSpace);
 		cp.add(send);
-
 		frame.add(cp, BorderLayout.SOUTH);
-
 	}
 
 	// getting the mouse position when clicked
 	int x, y;
 
-	public DrawingOvals() {
+	public DrawingOvalsServer() {
 
-		// chat client code
+		// chat server code
 		try {
+			serverSocket = new ServerSocket(8000);
+			socket = serverSocket.accept();
 
-			socket = new Socket("localhost", 8000);
 			in = new BufferedReader(new InputStreamReader(socket
 					.getInputStream(), "UTF8"));
 			out = new BufferedWriter(new OutputStreamWriter(socket
@@ -91,13 +87,18 @@ public class DrawingOvals extends JFrame implements Runnable {
 
 		send.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 				recieveSpace.append(" you : " + sendSpace.getText() + "\n");
+
 				try {
+
 					out.write(sendSpace.getText());
 					out.newLine();
 					out.flush();
 					sendSpace.setText("");
+
 				} catch (IOException ie) {
+
 				}
 			}
 		});
@@ -117,7 +118,6 @@ public class DrawingOvals extends JFrame implements Runnable {
 	// Painting the Circles and filling them
 	public void paint(Graphics g) {
 		super.paint(g);
-
 		// Drawing the Circles
 		for (int i = 1; i <= 7; i++) {
 			for (int j = 1; j <= 6; j++) {
@@ -125,7 +125,6 @@ public class DrawingOvals extends JFrame implements Runnable {
 				int ovalX = 130 + (i * 60);
 				int ovalY = (j * 60);
 				g.drawOval(ovalX, ovalY, 50, 50);
-
 				if (x >= ovalX && x <= 50 + ovalX) {
 					if (y >= ovalY && y <= 50 + ovalY) {
 						positions.add(i); // we have to change this "i" and put
