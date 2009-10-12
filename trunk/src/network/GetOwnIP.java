8d96924f723ed38bf4ddf703d15b5e8d80d88b5c
boody.fci <boody.fci@1202452c-5e50-11de-b59c-372aa07dcf7f>
2009-10-12 17:43:24 +0000

    Refined the code and Modified GetOwnIP.java and NewJFrame.java to work better

--- trunk/src/network/GetOwnIP.java
+++ trunk/src/network/GetOwnIP.java
@@ -12,7 +12,7 @@ public class GetOwnIP {
 	 * */
 	public static PortScanner x;
 	private String[] scannedIPs;
-	
+
 	public GetOwnIP() throws Exception {
 		int[] localIP = new int[4];
 		String[] temp = new String[4];
@@ -23,29 +23,24 @@ public class GetOwnIP {
 			NetworkInterface iface = ifaces.nextElement();
 			// This loop gets the Addresses for the interface (IP and MAC
 			// Addresses)
-			if (ifaces.hasMoreElements() == false) break;
-			for (Enumeration<InetAddress> addresses = iface.getInetAddresses(); addresses.hasMoreElements()/*hasOneElement*/;) {
-				/*
-				 * ERROR: hasMoreElements() is always true if there's one or
-				 * more element in iface
-				 */
-
+			if (ifaces.hasMoreElements() == false)
+				break;
+			for (Enumeration<InetAddress> addresses = iface.getInetAddresses(); addresses
+					.hasMoreElements();) {
 				// As we won't be working on the localhost only, we'll ignore
 				// the Loop Back address
 				if (iface.isLoopback() == false) {
 					InetAddress address = addresses.nextElement();
 					myStr = address.toString();
 				}
-				if (addresses.hasMoreElements() == false) break;
+				if (addresses.hasMoreElements() == false)
+					break;
 			}
 			// omitting the unnecessary character "/"
 			myStr = myStr.substring(1);
 			// Splitting the IP Address with the character "."
 			temp = myStr.split("[.]");
-			for (int i = 0; i < temp.length; i++) {
-				int Obj2 = Integer.parseInt(temp[i]);
-				localIP[i] = Obj2;
-			}
+			localIP = toIntegerIP(temp);
 			x = new PortScanner(localIP);
 			scannedIPs = x.IPsList.toArray(new String[0]);
 		}
@@ -53,5 +48,14 @@ public class GetOwnIP {
 
 	public String[] getScannedIPs() {
 		return scannedIPs;
+	}
+
+	public int[] toIntegerIP(String[] stringIP) {
+		int[] integerIP = new int[4];
+		for (int i = 0; i < stringIP.length; i++) {
+			int Obj2 = Integer.parseInt(stringIP[i]);
+			integerIP[i] = Obj2;
+		}
+		return integerIP;
 	}
 }
